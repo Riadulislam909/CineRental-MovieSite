9c3e131e3beeeb5f63fab9793957a8fc0304fffc
Riadulislam909 <riadulislam814@gmail.com>
2024-02-20 02:06:05 +0600

    convert HTML tamplete into components and showing data from MovieList

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+import MovieList from "./components/cine/MovieList";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <SideBar />
+          <MovieList />
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
